c55b8c4b597156de0a345ab76a4a903fdb981c0f
CernunnosYQ <yairquintanamx@gmail.com>
2022-12-13 08:09:16 -0600

    Adding colors to hints

--- Tile.py
+++ Tile.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 class Tile:
     def __init__(self, container, value, x, y, action):
+        self.colors = ["#2F2F2F", "#00BF96", "#4FBF2A", "#BFA300", "#BF7000", "#BF1C13"]
         self.container = container
         self.value = int(value)
         self.x, self.y = x, y
@@ -14,6 +15,11 @@ class Tile:
         self.btn["state"] = "disabled"
         self.btn["relief"] = "groove"
         self.btn["text"] = self.value
+
+        if self.value > 4:
+            self.btn.configure(disabledforeground=self.colors[5])
+        else:
+            self.btn.configure(disabledforeground=self.colors[self.value])
 
         self.action(self.x, self.y)
         
