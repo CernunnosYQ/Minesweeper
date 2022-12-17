34ebfb36f0af08d2d61072f4f434a89046cfe3c9
CernunnosYQ <yairquintanamx@gmail.com>
2022-12-17 17:30:43 -0600

    Now allows you to mark the mines

--- Tile.py
+++ Tile.py
@@ -7,21 +7,31 @@ class Tile:
         self.value = int(value)
         self.x, self.y = x, y
         self.action = action
+        self.state = 0
+        self.states = ['  ', '!', '?']
+        self.bg_colors = ['SystemButtonFace', '#BFA300', '#4FBF2A']
 
         self.btn = tk.Button(container, text="  ", command=self.click)
+        self.btn.bind("<Button-3>", self.rClick)
         self.btn.grid(row=y, column=x, sticky="news")
 
     def click(self):
-        self.btn["state"] = "disabled"
-        self.btn["relief"] = "groove"
-        self.btn["text"] = self.value
+        if self.state == 0:
+            self.btn["state"] = "disabled"
+            self.btn["relief"] = "groove"
+            self.btn["text"] = self.value
 
-        if self.value > 4:
-            self.btn.configure(disabledforeground=self.colors[5])
-        else:
-            self.btn.configure(disabledforeground=self.colors[self.value])
+            if self.value > 4:
+                self.btn.configure(disabledforeground=self.colors[5])
+            else:
+                self.btn.configure(disabledforeground=self.colors[self.value])
 
-        self.action(self.x, self.y)
+            self.action(self.x, self.y)
+
+    def rClick(self, event):
+        self.state = (self.state+1) % 3
+        self.btn["text"] = self.states[self.state]
+        self.btn["background"] = self.bg_colors[self.state]
         
 if __name__ == "__main__":
     print("This is not a main module")
