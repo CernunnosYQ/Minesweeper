7cd131937923527f316dd61d45fe509c41e9143a
CernunnosYQ <yairquintanamx@gmail.com>
2022-12-19 23:59:31 -0600

    Adding marker to show mines left and time

--- main.py
+++ main.py
@@ -6,9 +6,25 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.geometry("500x520")
+        self.geometry("500x600")
         self.title("Minesweeper")
         self.resizable(False, False)
+
+        self.marker = tk.Frame(self)
+        self.marker.columnconfigure(0, weight=2)
+        self.marker.columnconfigure(1, weight=1)
+        self.marker.columnconfigure(2, weight=2)
+
+        mines_label = tk.Label(self.marker, text="000")
+        mines_label.grid(column=0, row=0, sticky="ew", padx=5, pady=5)
+
+        config_btn = tk.Button(self.marker, text="#")
+        config_btn.grid(column=1, row=0, sticky="ew", padx=5, pady=5)
+
+        timer_label = tk.Label(self.marker, text="000")
+        timer_label.grid(column=2, row=0, sticky="ew", padx=5, pady=5)
+
+        self.marker.pack(fill="both")
 
         self.board = Board(self, 20, 20, 40)
 
